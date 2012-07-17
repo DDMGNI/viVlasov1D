1cd240d47117446dfb0b98e52aae8ccf30dc7065
Michael Kraus <michael.kraus@ipp.mpg.de>
2012-07-17 15:00:57 +0200

    Initial Commit.

---
+++ diagnostics/movie.py
@@ -0,0 +1,174 @@
+'''
+Created on Apr 06, 2012
+
+@author: Michael Kraus (michael.kraus@ipp.mpg.de)
+'''
+
+import matplotlib as mpl
+
+import StringIO
+import argparse
+import os
+
+import numpy as np
+import h5py
+
+import core
+#import plot
+
+
+class movie(object):
+    '''
+    
+    '''
+
+
+    def __init__(self, hdf5_files, nMax=0, nTime=0, nPlot=1, vMax=0.0, cMax=False, cFac=1.0, write=False):
+        '''
+        Constructor
+        '''
+        
+        self.hdf5_files = []
+        self.ntMax      = nMax
+        
+        for file in hdf5_files.split(','):
+            print("  Opening %s" % (file))
+            self.hdf5_files.append(h5py.File(file, 'r'))
+        
+        print
+        
+        # read config file from HDF5 and create config object
+        cfg_str = self.hdf5_files[0]['runcfg'][:][0]
+        
+        cfg_io = StringIO.StringIO(cfg_str.strip())
+        cfg     = core.Config(cfg_io)
+        cfg_io.close()
+        
+        self.grid         = core.Grid                (hdf5_in=self.hdf5_files[0], ntMax=nMax, replay=True)
+        self.potential    = core.Potential           (self.grid, hdf5_in=self.hdf5_files[0], replay=True,
+                                                      poisson_const=cfg['solver']['poisson_const'])
+        self.hamiltonian  = core.Hamiltonian         (self.grid, hdf5_in=self.hdf5_files[0], replay=True)
+        self.distribution = core.DistributionFunction(self.grid, hdf5_in=self.hdf5_files[0], replay=True)
+        
+        for ifile in range(1, len(self.hdf5_files)):
+            self.grid.append_time(self.hdf5_files[ifile]['grid']['t'][1:])
+        
+        self.plot = PlotMovie(self.grid, self.distribution, self.hamiltonian, self.potential,
+                              nTime, nPlot, vMax, cMax, cFac, write)
+        
+    
+    def __del__(self):
+        if self.hdf5_files != None:
+            for hdf5 in self.hdf5_files:
+                hdf5.close()
+        
+    
+    def init(self):
+        self.update(0)
+    
+    
+    def update(self, itime, final=False):
+        self.potential.read_from_hdf5(itime)
+        self.distribution.read_from_hdf5(itime)
+        self.hamiltonian.read_from_hdf5(itime)
+        
+        if itime > 0:
+            self.plot.add_timepoint()
+        
+        return self.plot.update(final=final)
+    
+    
+    def run(self, write=False):
+        ttime = 0
+        
+        for hdf5 in self.hdf5_files:
+            nt = len(hdf5['grid']['t'][:]) - 1
+            
+            self.potential.set_hdf5_file(hdf5)
+            self.hamiltonian.set_hdf5_file(hdf5)
+            self.distribution.set_hdf5_file(hdf5)
+    
+            for itime in range(1, nt+1):
+                if self.ntMax > 0 and ttime >= self.ntMax:
+                    break
+                
+                self.update(itime, final=(itime == nt and hdf5 == self.hdf5_files[-1]))
+                ttime += 1
+        
+    
+    def movie(self, outfile, fps=10):
+        self.run(write=True)
+        
+        command = ('mencoder',
+                   'mf://*.png',
+                   '-mf',
+                   'type=png:w=980:h=630:fps=' + str(fps),
+                   '-ovc',
+                   'x264',
+#                   'lavc',
+#                   '-lavcopts',
+#                   'vcodec=mpeg4', 
+                   '-oac',
+                   'copy',
+                   '-o',
+                   outfile)
+        
+        os.spawnvp(os.P_WAIT, 'mencoder', command)
+        
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Vlasov-Poisson Solver in 1D')
+    
+    parser.add_argument('hdf5_file', metavar='<run.hdf5>', type=str,
+                        help='Run HDF5 File')
+    parser.add_argument('-nt', metavar='i', type=int, default=0,
+                        help='plot i points in time traces')
+    parser.add_argument('-np', metavar='i', type=int, default=1,
+                        help='plot every i\'th frame')
+    parser.add_argument('-ntmax', metavar='i', type=int, default=0,
+                        help='limit to i points in time')
+    parser.add_argument('-v', metavar='f', type=float, default=0.0,
+                        help='limit velocity domain to +/-v')
+    parser.add_argument('-cmax', metavar='b', type=bool, default=False,
+                        help='use max values of simulation in contour plots')
+    parser.add_argument('-cfac', metavar='f', type=float, default=1.0,
+                        help='multiply max value of initial data in contour plots')
+    parser.add_argument('-o', metavar='<run.mp4>', type=str, default=None,
+                        help='output video file')    
+    parser.add_argument('-fps', metavar='i', type=int, default=1,
+                        help='frames per second')    
+    
+    args = parser.parse_args()
+    
+    if args.o != None:
+        write = True
+        mpl.use('AGG')
+    else:
+        write = False
+    
+
+    from plot.plot_movie import PlotMovie
+    
+    
+    print
+    print("Replay run with " + args.hdf5_file)
+    print
+    
+    pyvp = movie(args.hdf5_file, nMax=args.ntmax, nTime=args.nt, nPlot=args.np,
+                 vMax=args.v, cMax=args.cmax, cFac=args.cfac, write=write)
+    
+    if not write:
+        print
+        raw_input('Hit any key to start replay.')
+        print
+    
+    if write:
+        pyvp.movie(args.o, args.fps)
+    else:
+        pyvp.run()
+    
+    print
+    print("Replay finished.")
+    print
+    
---
+++ diagnostics/replay.py
@@ -0,0 +1,154 @@
+'''
+Created on Apr 06, 2012
+
+@author: Michael Kraus (michael.kraus@ipp.mpg.de)
+'''
+
+import StringIO
+import argparse
+import numpy as np
+import h5py
+
+import matplotlib.animation as animation
+
+import core
+import plot
+
+
+class replay(object):
+    '''
+    
+    '''
+
+
+    def __init__(self, hdf5_file, nPlot=1, iStart=0):
+        '''
+        Constructor
+        '''
+        
+        self.iStart = iStart
+        self.nPlot  = nPlot
+        
+        self.hdf5 = h5py.File(hdf5_file, 'r')
+        
+        # read config file from HDF5 and create config object
+#        cfg_str = self.hdf5['runcfg'][:][0]
+#        
+#        cfg_io = StringIO.StringIO(cfg_str.strip())
+#        cfg    = core.Config(cfg_io)
+#        cfg_io.close()
+        
+        self.grid         = core.Grid                (hdf5_in=self.hdf5, replay=True)
+        self.potential    = core.Potential           (self.grid, hdf5_in=self.hdf5, replay=True,
+                                                      poisson_const=-1.)
+        self.hamiltonian  = core.Hamiltonian         (self.grid, hdf5_in=self.hdf5, replay=True)
+        self.distribution = core.DistributionFunction(self.grid, hdf5_in=self.hdf5, replay=True)
+        
+        self.potential.read_from_hdf5(iStart)
+        self.distribution.read_from_hdf5(iStart)
+        self.hamiltonian.read_from_hdf5(iStart)
+        
+        self.plot = plot.PlotSpecies(self.grid, self.distribution, self.hamiltonian, self.potential,
+                                     self.grid.nt, iStart, nPlot)
+        
+    
+    def __del__(self):
+        if self.hdf5 != None:
+            self.hdf5.close()
+        
+    
+    def init(self, iStart=0):
+        self.update(iStart)
+    
+    
+    def update(self, itime, final=False):
+        self.potential.read_from_hdf5(itime)
+        self.distribution.read_from_hdf5(itime)
+        self.hamiltonian.read_from_hdf5(itime)
+        
+        print("it = %5i" % (itime))
+        
+        
+        if itime > 0:
+#            Ekin  = self.hamiltonian.Ekin
+#            Ekin0 = self.hamiltonian.Ekin0
+#            Epot  = self.hamiltonian.Epot  - self.potential.E
+#            Epot0 = self.hamiltonian.Epot0 - self.potential.E0
+#            Epot_f  = self.hamiltonian.Epot  / 2.
+#            Epot0_f = self.hamiltonian.Epot0 / 2.
+#            Epot_p  = self.potential.E
+#            Epot0_p = self.potential.E0
+#            
+#            dEkin = Ekin - Ekin0
+#            dEpot = Epot - Epot0
+#            dEpot_f = Epot_f - Epot0_f
+#            dEpot_p = Epot_p - Epot0_p
+#            
+#            E    = Ekin  + Epot
+#            E0   = Ekin0 + Epot0
+#            E_f  = Ekin  + Epot_f
+#            E0_f = Ekin0 + Epot0_f
+#            E_p  = Ekin  + Epot_p
+#            E0_p = Ekin0 + Epot0_p
+#            
+#            E_err   = (E   - E0  ) / E0
+#            E_err_f = (E_f - E0_f) / E0_f
+#            E_err_p = (E_p - E0_p) / E0_p
+#            
+#            print
+#            print("      dEpot/dEkin = %24.16E,  dEpot(f)/dEkin = %24.16E,  dEpot(phi)/dEkin = %24.16E" % (dEpot/dEkin, dEpot_f/dEkin, dEpot_p/dEkin))
+#            print("      E_pot       = %24.16E,  E_pot(f)       = %24.16E,  E_pot(phi)       = %24.16E" % (Epot, Epot_f, Epot_p))
+#            print("      E_err       = %24.16E,  E_err(f)       = %24.16E,  E_err(phi)       = %24.16E" % (E_err, E_err_f, E_err_p))
+            
+            
+            self.plot.add_timepoint()
+        
+        return self.plot.update(final=final)
+    
+    
+    def run(self):
+        for itime in range(self.iStart+1, self.grid.nt+1):
+            self.update(itime, final=(itime == self.grid.nt))
+        
+    
+    def movie(self, outfile, fps=1):
+        self.plot.nPlot = 1
+        
+        ani = animation.FuncAnimation(self.plot.figure, self.update, np.arange(1, self.grid.nt+1), 
+                                      init_func=self.init, repeat=False, blit=True)
+        ani.save(outfile, fps=fps)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Vlasov-Poisson Solver in 1D')
+    
+    parser.add_argument('hdf5_file', metavar='<run.hdf5>', type=str,
+                        help='Run HDF5 File')
+    parser.add_argument('-np', metavar='i', type=int, default=1,
+                        help='plot every i\'th frame')    
+    parser.add_argument('-ns', metavar='i', type=int, default=0,
+                        help='start at frame i')    
+    parser.add_argument('-o', metavar='<run.mp4>', type=str, default=None,
+                        help='output video file')    
+    
+    args = parser.parse_args()
+    
+    print
+    print("Replay run with " + args.hdf5_file)
+    print
+    
+    pyvp = replay(args.hdf5_file, args.np, args.ns)
+    
+    print
+    raw_input('Hit any key to start replay.')
+    print
+    
+    if args.o != None:
+        pyvp.movie(args.o, args.np, args.ns)
+    else:
+        pyvp.run()
+    
+    print
+    print("Replay finished.")
+    print
+    
