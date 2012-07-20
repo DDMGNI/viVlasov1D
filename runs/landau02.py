e91d78cea2def401c8ecd35916d13ec57379568e
Michael Kraus <michael.kraus@ipp.mpg.de>
2012-07-20 16:26:12 +0200

    - fixed nonlinear Landau damping initial data script

--- runs/landau02.py
+++ runs/landau02.py
@@ -2,6 +2,15 @@
 import numpy as np
 
 
-def density(grid):
-    return 1. + 0.5 * np.cos(0.5 * (grid.xGrid - 0.5 * grid.L))
+def temperature(x):
+    pass
 
+
+def density(x, L):
+    density = 1. + 0.5 * np.cos(0.5 * (x - 0.5 * L))
+
+    return density
+
+
+def distribution(x, y):
+    pass
