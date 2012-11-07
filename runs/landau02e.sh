967b2c7ea23aef79d6551cb3b3b7f7248fcc4102
Michael Kraus <michael.kraus@ipp.mpg.de>
2012-11-07 12:01:57 +0100

    - another small change in the new Landau damping cases

--- runs/landau02e.sh
+++ runs/landau02e.sh
@@ -6,7 +6,7 @@
 #
 #$ -l h_cpu=24:00:00
 #
-#$ -pe mpich2_tok_devel 32
+#$ -pe mpich2_tok_devel 8
 #
 #$ -m e
 #$ -M michael.kraus@ipp.mpg.de
@@ -27,6 +27,7 @@ module load impi
 module load python27/python
 module load python27/numpy
 module load python27/mpi4py
+module load python27/petsc4py
 module load python27/cython
 
 
@@ -37,4 +38,4 @@ export PYTHONPATH=$RUN_DIR:$PYTHONPATH
 
 cd $RUN_DIR
 
-mpiexec -np 32 python petscvp1d.py runs/$RUNID.cfg
+mpiexec -np 8 python petscvp1d.py runs/$RUNID.cfg
