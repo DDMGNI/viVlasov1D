23fb5bad91e3d022294929a229110f3e6847716b
Michael Kraus <michael.kraus@ipp.mpg.de>
2013-01-24 22:57:45 +0100

    - another  fix in twostream init file

--- runs/twostream02.py
+++ runs/twostream02.py
@@ -5,10 +5,7 @@ from vlasov.data.maxwell import maxwellian
 
 
 def distribution(x, v):
-    f = maxwellian(1., v) * (1. + 0.05 * np.cos(0.5 * x))
-    
-    if v != 0.:
-        f *= v**2
+    f = maxwellian(1., v) * (1. + 0.05 * np.cos(0.5 * x)) * v**2
     
     return f 
 
