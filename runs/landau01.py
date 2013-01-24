dfa3bc07407aad732abd3c6bbb422ec6b57b3575
Michael Kraus <michael.kraus@ipp.mpg.de>
2013-01-24 08:29:15 +0100

    - fixed linear Landau damping init script

--- runs/landau01.py
+++ runs/landau01.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 
-def density(grid):
-    return 1. + 0.01 * np.cos(0.5 * (grid.xGrid - 0.5 * grid.L))
+def density(x, L):
+    density = 1. + 0.01 * np.cos(0.5 * (x - 0.5 * L))
 
+    return density
