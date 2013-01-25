- fixed sign in KEEN wave init script

runs/keen.py
@@ -15,6 +15,6 @@ def external(x, t):
     eps =   0.5 * ( np.tanh( (t0-tL)/tO ) - np.tanh( (t0-tR)/tO ) )
     a   = ( 0.5 * ( np.tanh( (t -tL)/tO ) - np.tanh( (t -tR)/tO ) ) - eps ) / (1.-eps) 
     
-    potential = Emax * a * np.cos(k*x - omega*t)
+    potential = - Emax * a * np.cos(k*x - omega*t)
     
     return potential