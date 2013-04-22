52b7ec4f0639f3395d53a58f07ffedb9c059e365
Michael Kraus <michael.kraus@ipp.mpg.de>
2013-04-22 13:39:57 +0200

    - correct path in tok and itm runscripts

--- runs_tokbc/benchmark_32_201x401.sh
+++ runs_tokbc/benchmark_32_201x401.sh
@@ -28,6 +28,6 @@ module load python32/all
 export LD_PRELOAD=/afs/@cell/common/soft/intel/ics13/13.0/mkl/lib/intel64/libmkl_core.so:/afs/@cell/common/soft/intel/ics13/13.0/mkl/lib/intel64/libmkl_intel_thread.so:/afs/@cell/common/soft/intel/ics13/13.0/compiler/lib/intel64/libiomp5.so
 
 
-mpiexec -n 32 python3.2 run_direct_nonlinear.py runs/$RUNID.cfg
+mpiexec -n 32 python3.2 run_direct_nonlinear.py runs_tokbc/$RUNID.cfg
 
 
