7607aa6eba256999adc4b634e6467e8705606536
Michael Kraus <michael.kraus@ipp.mpg.de>
2013-04-22 12:24:10 +0200

    - moved TOK cluster run files to separate directory

---
+++ runs_tokbc/jeans_damping01_201x401_dt1E-1_nu1E-3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#$ -cwd
+#
+#$ -l h_cpu=96:00:00
+#
+#$ -pe mpich2_tok_production 8
+#
+#$ -o /ptmp1/mkraus/petscVlasovPoisson1D/jeans_damping01_201x401_dt1E-1_nu1E-3.out
+#$ -e /ptmp1/mkraus/petscVlasovPoisson1D/jeans_damping01_201x401_dt1E-1_nu1E-3.err
+#
+#$ -m e
+#$ -M michael.kraus@ipp.mpg.de
+#
+#$ -notify
+#
+#$ -N viVlasov1D
+#
+
+
+RUNID=jeans_damping01_201x401_dt1E-1_nu1E-3
+
+
+module load intel/13.1
+module load mkl/11.0
+module load impi/4.1.0
+
+module load python32/all
+
+
+export LD_PRELOAD=/afs/@cell/common/soft/intel/ics13/13.0/mkl/lib/intel64/libmkl_core.so:/afs/@cell/common/soft/intel/ics13/13.0/mkl/lib/intel64/libmkl_intel_thread.so:/afs/@cell/common/soft/intel/ics13/13.0/compiler/lib/intel64/libiomp5.so
+
+
+mpiexec -n 8 python3.2 run_direct_nonlinear2.py runs/$RUNID.cfg
---
+++ runs_tokbc/landau02_201x401_dt1E-2_nu0_linear.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#$ -cwd
+#
+#$ -l h_cpu=96:00:00
+#
+#$ -pe mpich2_tok_production 8
+#
+#$ -o /ptmp1/mkraus/petscVlasovPoisson1D/landau02_alpha0_dt1e-2_linear.out
+#$ -e /ptmp1/mkraus/petscVlasovPoisson1D/landau02_alpha0_dt1e-2_linear.err
+#
+#$ -m e
+#$ -M michael.kraus@ipp.mpg.de
+#
+#$ -notify
+#
+#$ -N viVlasov1D
+#
+
+
+RUNID=landau02_201x401_dt1E-2_nu0_linear
+
+
+module load intel/13.1
+module load mkl/11.0
+module load impi/4.1.0
+
+module load python32/all
+
+
+export LD_PRELOAD=/afs/@cell/common/soft/intel/ics13/13.0/mkl/lib/intel64/libmkl_core.so:/afs/@cell/common/soft/intel/ics13/13.0/mkl/lib/intel64/libmkl_intel_thread.so:/afs/@cell/common/soft/intel/ics13/13.0/compiler/lib/intel64/libiomp5.so
+
+
+mpiexec -n 8 python3.2 run_direct_linear.py runs/$RUNID.cfg
+
+
