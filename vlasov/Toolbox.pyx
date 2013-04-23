2adf4e9f6b0bca6f34c5ab3999bf00bb5b71ca68
Michael Kraus <michael.kraus@ipp.mpg.de>
2013-04-23 11:42:24 +0200

    - disabled spatial averaging of collision operator in J1 Jacobian and Toolbox

--- vlasov/Toolbox.pyx
+++ vlasov/Toolbox.pyx
@@ -370,11 +370,13 @@ cdef class Toolbox(object):
         cdef np.ndarray[np.float64_t, ndim=1] v = self.v
         cdef np.float64_t result
         
-        result = 0.25 * ( \
-                          + 1. * ( (N[i-1] * v[j+1] - U[i-1]) * f[i-1, j+1] - (N[i-1] * v[j-1] - U[i-1]) * f[i-1, j-1] ) * A[i-1] \
-                          + 2. * ( (N[i  ] * v[j+1] - U[i  ]) * f[i,   j+1] - (N[i  ] * v[j-1] - U[i  ]) * f[i,   j-1] ) * A[i  ] \
-                          + 1. * ( (N[i+1] * v[j+1] - U[i+1]) * f[i+1, j+1] - (N[i+1] * v[j-1] - U[i+1]) * f[i+1, j-1] ) * A[i+1] \
-                        ) * 0.5 / self.hv
+#         result = 0.25 * ( \
+#                           + 1. * ( (N[i-1] * v[j+1] - U[i-1]) * f[i-1, j+1] - (N[i-1] * v[j-1] - U[i-1]) * f[i-1, j-1] ) * A[i-1] \
+#                           + 2. * ( (N[i  ] * v[j+1] - U[i  ]) * f[i,   j+1] - (N[i  ] * v[j-1] - U[i  ]) * f[i,   j-1] ) * A[i  ] \
+#                           + 1. * ( (N[i+1] * v[j+1] - U[i+1]) * f[i+1, j+1] - (N[i+1] * v[j-1] - U[i+1]) * f[i+1, j-1] ) * A[i+1] \
+#                         ) * 0.5 / self.hv
+        
+        result = ( (N[i  ] * v[j+1] - U[i  ]) * f[i,   j+1] - (N[i  ] * v[j-1] - U[i  ]) * f[i,   j-1] ) * A[i  ] * 0.5 / self.hv
         
         return result
     
@@ -389,11 +391,13 @@ cdef class Toolbox(object):
         
         cdef np.float64_t result
         
-        result = 0.25 * ( \
-                     + 1. * ( f[i-1, j+1] - 2. * f[i-1, j  ] + f[i-1, j-1] ) \
-                     + 2. * ( f[i,   j+1] - 2. * f[i,   j  ] + f[i,   j-1] ) \
-                     + 1. * ( f[i+1, j+1] - 2. * f[i+1, j  ] + f[i+1, j-1] ) \
-                 ) * self.hv2_inv
+#         result = 0.25 * ( \
+#                      + 1. * ( f[i-1, j+1] - 2. * f[i-1, j  ] + f[i-1, j-1] ) \
+#                      + 2. * ( f[i,   j+1] - 2. * f[i,   j  ] + f[i,   j-1] ) \
+#                      + 1. * ( f[i+1, j+1] - 2. * f[i+1, j  ] + f[i+1, j-1] ) \
+#                  ) * self.hv2_inv
+        
+        result = ( f[i,   j+1] - 2. * f[i,   j  ] + f[i,   j-1] ) * self.hv2_inv
         
         return result
 
