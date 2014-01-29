75ff3e7d71e6cf6d23887bfb1a34fc79a17c65f9
Michael Kraus <michael.kraus@ipp.mpg.de>
2014-01-29 12:11:33 +0100

    - updated TOKP runscripts to use run.py and SGE $JOB_ID

--- runs_tokp/jeans_strong_512x1024_dt1E-1_nu0.sh
+++ runs_tokp/jeans_strong_512x1024_dt1E-1_nu0.sh
@@ -7,8 +7,8 @@
 #$ -P  tokp
 #$ -pe impi_hydra 16
 #
-#$ -o /tokp/scratch/mkraus/petscVlasovPoisson1D/jeans_strong_512x1024_dt1E-1_nu0.out
-#$ -e /tokp/scratch/mkraus/petscVlasovPoisson1D/jeans_strong_512x1024_dt1E-1_nu0.err
+#$ -o /tokp/scratch/mkraus/petscVlasovPoisson1D/jeans_strong_512x1024_dt1E-1_nu0.$JOB_ID.out
+#$ -e /tokp/scratch/mkraus/petscVlasovPoisson1D/jeans_strong_512x1024_dt1E-1_nu0.$JOB_ID.err
 #
 #$ -m e
 #$ -M michael.kraus@ipp.mpg.de
@@ -43,6 +43,4 @@ export LD_PRELOAD=/afs/@cell/common/soft/intel/ics2013/14.0/mkl/lib/intel64/libm
 export LD_PRELOAD=/afs/@cell/common/soft/intel/ics2013/14.0/compiler/lib/intel64/libiomp5.so:$LD_PRELOAD
 
 
-mpiexec -perhost 16 -l -n 16 python3.3 run_nonlinear_matrixfree_split_pc_kinetic_tensor.py -c runs_tokp/$RUNID.cfg
-
-#mpiexec -perhost 16 -l -n 16 python3.3 run_nonlinear_matrixfree_split.py -c runs_tokp/$RUNID.cfg
+mpiexec -perhost 16 -l -n 16 python3.3 run.py -c runs_tokp/$RUNID.cfg -i $JOB_ID
