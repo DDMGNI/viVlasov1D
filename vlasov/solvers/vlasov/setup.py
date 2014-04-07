38ff01eba9ab2659b2504f064b3da2c2f212764d
Michael Kraus <michael.kraus@ipp.mpg.de>
2014-04-07 09:20:04 +0200

    - removed some more old files

--- vlasov/solvers/vlasov/setup.py
+++ vlasov/solvers/vlasov/setup.py
@@ -35,127 +35,6 @@ extensions = [
                   extra_link_args=LARGS
                  ),
                
-#         Extension("PETScNLVlasovTensorFastMP",
-#                   sources=["PETScNLVlasovTensorFastMP.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-               
-#         Extension("PETScNLVlasovArakawaJ1DB2",
-#                   sources=["PETScNLVlasovArakawaJ1DB2.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ1DBf",
-#                   sources=["PETScNLVlasovArakawaJ1DBf.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ1DBh",
-#                   sources=["PETScNLVlasovArakawaJ1DBh.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-              
-#         Extension("PETScNLVlasovArakawaJ4kinetic",
-#                   sources=["PETScNLVlasovArakawaJ4kinetic.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ4potential",
-#                   sources=["PETScNLVlasovArakawaJ4potential.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-               
-#         Extension("PETScNLVlasovArakawaJ1TensorFast",
-#                   sources=["PETScNLVlasovArakawaJ1TensorFast.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ1AveMTensorFast",
-#                   sources=["PETScNLVlasovArakawaJ1AveMTensorFast.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ1AveSTensorFast",
-#                   sources=["PETScNLVlasovArakawaJ1AveSTensorFast.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ4TensorFast",
-#                   sources=["PETScNLVlasovArakawaJ4TensorFast.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ4TensorSciPy",
-#                   sources=["PETScNLVlasovArakawaJ4TensorSciPy.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ4AveATensorFast",
-#                   sources=["PETScNLVlasovArakawaJ4AveATensorFast.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-#         Extension("PETScNLVlasovArakawaJ4AveSTensorFast",
-#                   sources=["PETScNLVlasovArakawaJ4AveSTensorFast.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  ),
-        
         Extension("PETScNLVlasovArakawaJ4RK2",
                   sources=["PETScNLVlasovArakawaJ4RK2.pyx"],
                   include_dirs=INCLUDE_DIRS,
@@ -165,6 +44,7 @@ extensions = [
                   extra_compile_args=CARGS,
                   extra_link_args=LARGS
                  ),
+        
         Extension("PETScNLVlasovArakawaJ4RK4",
                   sources=["PETScNLVlasovArakawaJ4RK4.pyx"],
                   include_dirs=INCLUDE_DIRS,
@@ -174,17 +54,6 @@ extensions = [
                   extra_compile_args=CARGS,
                   extra_link_args=LARGS
                  )
-        
-#         Extension("PETScNLVlasovSimpson",
-#                   sources=["PETScNLVlasovSimpson.pyx"],
-#                   include_dirs=INCLUDE_DIRS,
-#                   libraries=LIBRARIES,
-#                   library_dirs=LIBRARY_DIRS,
-#                   runtime_library_dirs=LIBRARY_DIRS,
-#                   extra_compile_args=CARGS,
-#                   extra_link_args=LARGS
-#                  )
-        
               ]
                 
 setup(
