9d040aa2af3a94deddcfb7f1d6a51ae84dc32ae8
Michael Kraus <michael.kraus@ipp.mpg.de>
2014-10-17 10:48:02 +0200

    - dito

---
+++ runs/twostream_bgk3.py
@@ -0,0 +1,19 @@
+"""
+Twostream Instability
+
+   Initial distribution function:
+   
+   :math:`f(x,v) = v^2 \, f_{M} \, ( 1 + A \, \cos{( k_{x} \, x) } )`
+   with :math:`A = 0.05, k = 0.5`
+"""
+
+import numpy as np
+
+from vlasov.toolbox.maxwell import maxwellian
+
+
+def distribution(x, v):
+    f = maxwellian(4., v) * (1. + 0.05 * np.cos(0.5 * x)) * v**2
+    
+    return f 
+
