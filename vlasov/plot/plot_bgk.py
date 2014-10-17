07f7940eced036d539e6b93cdc1ec255b049e29f
Michael Kraus <michael.kraus@ipp.mpg.de>
2014-10-17 10:53:20 +0200

    - put some analysis in bgk plot

--- vlasov/plot/plot_bgk.py
+++ vlasov/plot/plot_bgk.py
@@ -4,6 +4,8 @@ Created on Mar 21, 2012
 @author: mkraus
 '''
 
+import numpy as np
+
 import matplotlib.pyplot as plt
 
 
@@ -50,9 +52,60 @@ class PlotBGK(object):
         self.title = self.figure.text(0.5, 0.97, 't = %1.2f' % (grid.t[self.iTime]), horizontalalignment='center') 
         
         # create scatter plot
-        plt.scatter(self.h, self.f)
+        plt.scatter(self.h, self.f, marker='o', c='b', s=1)
         plt.xlabel('Total Energy')
         plt.ylabel('Distribution Function')
+        plt.xlim((-3,+12))
+#        plt.xlim((-5,+35))
+        
+        
+        print()
+        print("BGK Analysis")
+        print("============")
+        print()
+        
+        fmax = self.distribution.f.max()
+        hmin = self.hamiltonian.h.min()
+        
+        fi = 0
+        fj = 0
+        hi = 0
+        hj = 0
+        
+        for i in range(self.grid.nx):
+            for j in range(self.grid.nv):
+                if self.distribution.f[i,j] == fmax:
+                    fi = i
+                    fj = j
+                if self.hamiltonian.h[i,j] == hmin:
+                    hi = i
+                    hj = j
+        
+        slope = (self.distribution.f[fi,fj] - self.distribution.f[hi,hj]) \
+              / (self.hamiltonian.h[fi,fj]  - self.hamiltonian.h[hi,hj])
+        
+        print("min(phi):  %16.8E" % np.min(self.hamiltonian.h1))
+        print("max(phi):  %16.8E" % np.max(self.hamiltonian.h1))
+        print()
+        print("min(h):    %16.8E" % hmin)
+        print("x(min(h)): %16.8E" % self.grid.x[hi])
+        print("v(min(h)): %16.8E" % self.grid.v[hj])
+        print()
+        print("max(f):    %16.8E" % fmax)
+        print("x(max(f)): %16.8E" % self.grid.x[fi])
+        print("v(max(f)): %16.8E" % self.grid.v[fj])
+        print("h(max(f)): %16.8E" % self.hamiltonian.h[fi,fj])
+        print()
+        print("slope:     %16.8E" % slope)
+        print("slope^-1:  %16.8E" % (1./slope))
+        print()
+        
+        print()
+        print()
+        
+        
+        plt.plot([self.hamiltonian.h[hi,hj], self.hamiltonian.h[fi,fj]], [self.distribution.f[hi,hj], self.distribution.f[fi,fj]], color='r')
+        
         
         if self.write:
             filename = self.prefix + str('%06d' % (self.iTime)) + '.png'
