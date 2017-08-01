8f474560d88fa4c308fea35844d6d972c845da65
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-08-01 14:31:20 +0200

    Add TOKP template run files and run file generation script.

---
+++ runs_tokp/generate.py
@@ -0,0 +1,90 @@
+
+from vlasov.core.config  import Config
+
+
+# test cases
+run_ids = [
+        ("landau_linear", "Linear Landau Damping"),
+        ("landau_nonlinear", "Nonlinear Landau Damping"),
+        ("twostream", "Twostream Instability"),
+        ("twostream_shifted", "Twostream Instability (shifted)"),
+        ("bump_on_tail", "Bump-on-tail"),
+        ("jeans_damping", "Jeans Damping"),
+        ("jeans_weak", "Weak Jeans Instability"),
+        ("jeans_strong", "Strong Jeans Instability"),
+    ]
+
+# grid resolution (nx,nv)
+resolutions = [
+        ( 64, 128),
+        (128, 256),
+        (256, 512),
+        (512,1024),
+    ]
+
+# collision frequencies
+collisions = [
+        0E+0,
+        1E-5,
+        1E-4,
+        2E-4,
+        4E-4,
+    ]
+
+# run file directory
+run_dir = "runs_tokp"
+
+# run script filename
+run_filename = "submit_paper_runs.sh"
+
+# run script header
+run_script = """
+#!/bin/bash
+"""
+
+
+# loop over all test cases
+for (run_id, run_title) in run_ids:
+    # read template config file
+    cfg = Config(run_dir + "/" + run_id + "/" + run_id + ".cfg")
+    
+    # add section to run script
+    run_script += "\n"
+    run_script += "# " + run_title + "\n"
+    run_script += "\n"
+    
+    # loop over all grid resolutions
+    for (nx,nv) in resolutions:
+        # loop over all collision frequencies
+        for nu in collisions:
+            
+            # set grid resolution
+            cfg['grid']['nx'] = nx
+            cfg['grid']['nv'] = nv
+            
+            # set collision frequency
+            cfg['solver']['coll_freq'] = nu
+            
+            # compose run name
+            run_name = run_id \
+                     + "_" + str(nx) + "x" + str(nv) \
+                     + "_nu%1.0E" % nu
+            
+            # compose config file name
+            outfile = run_dir + "/" + run_id + "/" +  run_name + ".cfg"
+            
+            # create config files for each run
+            cfg.write_current_config(outfile)
+            
+            # add entry in run script
+            run_script += "qsub -N " + run_id + "/" +run_name + " runs_tokp/run_tokp.sh"
+            run_script += "\n"
+        
+        run_script += "\n"
+            
+
+f = open(run_filename, "w")
+f.write(run_script)
+f.close()
+
+print(run_script)
